666fafe9d058234769dbcb72c0c84573c0c3b9dd
Jesús Carrasquilla <jacvcarrasquilla@gmail.com>
2024-07-15 18:19:06 -0400

    Inicial

---
+++ tareas.js
@@ -0,0 +1,69 @@
+(function(){
+
+    // variable
+    
+    var lista=document.getElementById("lista"),
+        listaok=document.getElementById("tarealista"),
+        agregar=document.getElementById("agregar"), 
+        boton=document.getElementById("btn-tarea")
+
+    // funciones
+
+    var incluir = function(){
+
+        if (agregar.value != "") {
+            var tarea = agregar.value,
+                nuevoLi = document.createElement("li"),
+                nuevoA = document.createElement("a"),
+                contenido = document.createTextNode(tarea)
+                nuevoA.appendChild(contenido)
+                nuevoA.setAttribute("href" , "#")
+                nuevoLi.appendChild(nuevoA)
+                nuevoLi.setAttribute("id", tarea)
+                document.getElementById("lista").appendChild(nuevoLi)
+                // ---------------
+                nuevoInput = document.createElement("input")
+                nuevoInput.setAttribute("type", "button")
+                nuevoInput.setAttribute("value", "Eliminar")
+                nuevoInput.className = "btn-del"
+                nuevoLi.appendChild(nuevoInput)
+                // ---------------
+                nuevoInput = document.createElement("input")
+                nuevoInput.setAttribute("type", "button")
+                nuevoInput.setAttribute("value", " OK ")
+                nuevoInput.className = "btn-ok"
+                nuevoLi.appendChild(nuevoInput)
+                // ---------------
+                let i=lista.children.length-1
+                // ---------------
+                let btndel = document.getElementsByClassName("btn-del")
+                btndel[i].addEventListener("click", eliminar)
+                // ---------------
+                let btnok = document.getElementsByClassName("btn-ok")
+                btnok[i].addEventListener("click", ok)
+                // ----------------
+                agregar.value = ""
+        } else alert("Por favor ingrese tarea a agregar.")  
+    };
+
+    var eliminar = function(){
+        let idpadre = this.parentNode.getAttribute("id")
+        let padre = document.getElementById(idpadre)
+        padre.parentNode.removeChild(padre)
+    };
+
+    var ok = function(){
+        let idpadre = this.parentNode.getAttribute("id")
+        let padre = document.getElementById(idpadre)
+        padre.parentNode.removeChild(padre)
+        document.getElementById("tarealista").appendChild(padre)
+
+        padre = document.getElementById(idpadre)
+        padre.removeChild(padre.lastElementChild)
+    }
+    
+    // eventos
+    
+    boton.addEventListener("click", incluir)
+    
+}());
